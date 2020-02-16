a9db927b79ced88fbc7adc313216fed91aea1f10
Diego Ulloa <diegoulloao@icloud.com>
2020-02-16 12:44:00 -0300

    Seeder added

---
+++ src/database/seeders/user.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ *  Users Seeder
+ *  @seeder @db
+ * 
+ */
+
+
+'use strict';
+const faker = require( 'faker' )
+
+
+// You can set your locate with: faker.setLocale( 'es' )
+
+module.exports = {
+
+    up: ( queryInterface, Sequelize ) => {
+
+        let data = []
+        
+        const date = new Date()
+        let amount = 10
+
+        while( amount-- )
+
+            data.push({
+
+                name: faker.name.firstName(),
+                lastname: faker.name.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                createdAt: date,
+                updatedAt: date
+
+            })
+        ;
+
+
+        return queryInterface.bulkInsert( 'Users', data, {} )
+
+    },
+
+    down: ( queryInterface, Sequelize ) => {
+          return queryInterface.bulkDelete( 'Users', null, {} )
+    }
+
+}
