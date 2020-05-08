cd9a3c6db9ba205c2a652c5dc17fa8c003b0833c
Diego Ulloa <diegoulloao@icloud.com>
2020-05-08 02:05:08 -0400

    CI test workflow

--- src/providers/config/sequelize-cli.conf.js
+++ src/providers/config/sequelize-cli.conf.js
@@ -49,9 +49,9 @@ let SequelizeCLIOptions = {
         host: process.env.TEST_DB_HOST,
         port: process.env.TEST_DB_PORT,
 
-        username: process.env.DB_TEST_USERNAME,
-        password: process.env.DB_TEST_PASSWORD,
-        database: process.env.DB_TEST_DATABASE,
+        username: process.env.TEST_DB_USERNAME,
+        password: process.env.TEST_DB_PASSWORD,
+        database: process.env.TEST_DB_DATABASE,
 
         dialectOptions: {
             bigNumberStrings: true
@@ -69,9 +69,9 @@ let SequelizeCLIOptions = {
         host: process.env.PROD_DB_HOST,
         port: process.env.PROD_DB_PORT,
 
-        username: process.env.DB_PROD_USERNAME,
-        password: process.env.DB_PROD_PASSWORD,
-        database: process.env.DB_PROD_DATABASE,
+        username: process.env.PROD_DB_USERNAME,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_DATABASE,
 
         dialectOptions: {
             bigNumberStrings: true,
