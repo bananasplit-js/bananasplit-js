80b12c13cca293f95d7c4b43f2e4e03002c9d635
Diego Ulloa <diegoulloao@icloud.com>
2020-09-09 19:37:13 -0300

    Jest config file added src/providers to ignore list in --coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
+/**
+ * 
+ *  Jest configuration file
+ *  @conf
+ * 
+ */
+
 const { defaults } = require( 'jest-config' )
 
 module.exports = {
     ... defaults,
     roots: [ "<rootDir>/src/tests" ],
+    coveragePathIgnorePatterns: [ "/node_modules/", "<rootDir>/src/providers" ],
     preset: 'ts-jest',
 }
