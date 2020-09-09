d15a400fc08df63c779440c650b0bdc46565f4db
Diego Ulloa <diegoulloao@icloud.com>
2020-09-09 15:07:11 -0300

    app.ts -> services.ts / index.ts -> app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,19 @@
 /**
  * 
- *  App
+ *  Bananasplit-js for Express
  * 
  *  @module app
- *  @description This is your Bananasplit App!
+ *  @description All begins here
  * 
  */
 
 
-
-import Express from './providers/express'
-import Apollo from './providers/apollo'
+import { express, apollo } from './services'
 
 
+; ( async () => {
 
-/* ----------------------------------------------------------------
- *  
- *  This is your Bananasplit App!
- *  
- *  You can specify ports
- *      Express.provide({ port: 7000 }) or express.start( 7000 )
- *  
- * -----------------------------------------------------------------*/
+    // Start the services
+    await express.start()
 
-
-// Express Server Provider
-const express: Express = Express.provide()
-
-// Apollo Server Provider
-const apollo: Apollo = Apollo.provide({
-    middleware: express
-})
-
-
-export { express, apollo }
+})()
---
+++ src/services.ts
@@ -0,0 +1,34 @@
+/**
+ * 
+ *  Services
+ * 
+ *  @module services
+ *  @description Services provided by Bananasplit
+ * 
+ */
+
+
+
+import Express from './providers/express'
+import Apollo from './providers/apollo'
+
+
+
+/* ----------------------------------------------------------------
+ *  
+ *  You can specify ports
+ *      Express.provide({ port: 7000 }) or express.start( 7000 )
+ *  
+ * -----------------------------------------------------------------*/
+
+
+// Express Server Provider
+const express: Express = Express.provide()
+
+// Apollo Server Provider
+const apollo: Apollo = Apollo.provide({
+    middleware: express
+})
+
+
+export { express, apollo }
