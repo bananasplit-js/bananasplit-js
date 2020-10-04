589dd4d0177064385949fa77dd12d2405bc32139
Diego Ulloa <diegoulloao@icloud.com>
2020-10-03 23:08:46 -0300

    Database: Factories

--- src/database/seeders/user-table-seeder.js
+++ src/database/seeders/user-table-seeder.js
@@ -1,43 +1,24 @@
 /**
  * 
  *  Users seeder
- *  @description seeds user table with fake data
+ *  @description seeds the user table with fake data
  * 
  */
 'use strict';
 
-const faker = require( 'faker' )
 
-
-/**
- * 
- *  You can set your locate with:
- *  faker.setLocale('es')
- * 
- */
+const createUser = require( '../factories/create-user' )
 
 
 module.exports = {
 
     up: ( queryInterface, Sequelize ) => {
 
-        let data = []
-        
-        const date = new Date
         let amount = 10
+        let data = []
 
         while ( amount-- )
-
-            data.push({
-
-                name: faker.name.firstName(),
-                lastname: faker.name.lastName(),
-                email: faker.internet.email(),
-                password: faker.internet.password(),
-                createdAt: date,
-                updatedAt: date
-
-            })
+            data.push( createUser() )
         ;
 
 
