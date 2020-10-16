8209a7086557c67fda88e0b056446adf7fd34b99
Diego Ulloa <diegoulloao@icloud.com>
2020-10-16 08:18:05 -0300

    Sequelize client: options @core and @custom

--- src/providers/core/config/sequelize-cli.conf.js
+++ src/providers/core/config/sequelize-cli.conf.js
@@ -1,9 +1,9 @@
 /**
  * 
- *  Sequelize-cli configuration
+ *  Sequelize Client: options
  *  @config
  * 
- *  @description contains all sequelize-cli configurations
+ *  @description manage default and custom sequelize client options
  *
  */
 import dotenv from 'dotenv'
@@ -15,8 +15,9 @@ dotenv.config()
 
 export default {
 
-    // For development
-
+    /**
+     *  @development 
+     */
     development: {
 
         dialect: eval(`"${process.env.DB_DIALECT}"`),
@@ -37,18 +38,19 @@ export default {
     },
 
 
-    // For testing
-
+    /**
+     *  @test
+     */
     test: {
 
-        dialect: eval(`"${process.env.TEST_DB_DIALECT}"`),
+        dialect: eval(`"${process.env.DB_DIALECT}"`),
 
-        host: process.env.TEST_DB_HOST,
-        port: process.env.TEST_DB_PORT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
 
-        username: process.env.TEST_DB_USERNAME,
-        password: process.env.TEST_DB_PASSWORD,
-        database: process.env.TEST_DB_DATABASE,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
 
         dialectOptions: {
             bigNumberStrings: true
@@ -58,19 +60,20 @@ export default {
 
     },
 
-    
-    // For production
-    
+
+    /**
+     *  @production
+     */
     production: {
 
-        dialect: eval(`"${process.env.PROD_DB_DIALECT}"`),
+        dialect: eval(`"${process.env.DB_DIALECT}"`),
 
-        host: process.env.PROD_DB_HOST,
-        port: process.env.PROD_DB_PORT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
 
-        username: process.env.PROD_DB_USERNAME,
-        password: process.env.PROD_DB_PASSWORD,
-        database: process.env.PROD_DB_DATABASE,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
 
         dialectOptions: {
             bigNumberStrings: true,
