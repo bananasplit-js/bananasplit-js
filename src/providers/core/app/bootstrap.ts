00f76eea74d72eb9eb155cc4b9a8e7845c38fd89
Diego Ulloa <diegoulloao@icloud.com>
2020-10-16 16:55:03 -0300

    Bootstrap: logic rewrite

--- src/providers/core/app/bootstrap.ts
+++ src/providers/core/app/bootstrap.ts
@@ -7,7 +7,7 @@
  * 
  */
 import chalk from 'chalk'
-import { servicesLog } from '../helpers'
+import { servicesLog } from '@core/helpers'
 
 
 interface IStack {
@@ -18,37 +18,25 @@ interface IStack {
 /**
  * 
  *  Stack
- *  @description builds a stack to serve the services
+ *  @description receives a stack and run each service
  * 
  *  @param { any[] } services - an array of services
  *  @returns { IStack }
  * 
  */
-const Stack = ( services: any[] ): IStack => ({
+export default ( services: any[] ): IStack => ({
     
-    async serve(): Promise <void> {
+    serve(): void {
         let output: string[] = []
 
-        for ( const service of services ) {
+        services.forEach( async service => {
             let host: string = `http://localhost:${service.port}`
+            service.middleware ? host=service.path : await service.serve()
 
-            if ( !service.middleware )
-                await service.serve()
-
-            else
-                host = service.path
-            ;
-            
             output.push( `${chalk.bold.cyan(`- ${service.name} →`)} ${chalk.red(host)}` )
-        }
-
+        })
         
-        // SERVICES STATUS
-        servicesLog( output.join('\n') )
-        
+        servicesLog( output.join('\n') )        
     }
 
 })
-
-
-export default Stack
