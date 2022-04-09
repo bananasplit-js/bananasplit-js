778d6ac40c1c0ab12a2b30eb97f33728c106920d
Diego <diegoulloao@icloud.com>
2022-04-09 08:01:59 -0400

    feat: test transpiled to dist

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,12 @@
  * 
  */
 
-const hq = require( "alias-hq" )
-const { defaults } = require( "jest-config" )
+const { defaults } = require("jest-config")
 
 module.exports = {
 	...defaults,
-	roots: [ "<rootDir>/tests" ],
-	coveragePathIgnorePatterns: [ "/node_modules/", "<rootDir>/src/providers" ],
+	roots: ["<rootDir>/tests"],
+	coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/src/providers"],
 	preset: "ts-jest",
-	moduleNameMapper: hq.get( "jest" )
+	moduleNameMapper: require("alias-hq").get("jest")
 }
