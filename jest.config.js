01286d1693c300e72337abbb6d041a0e5d493825
Diego <diegoulloao@icloud.com>
2022-04-10 02:04:25 -0400

    feat: build stack clean / jest config ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,10 @@ module.exports = {
 	...defaults,
 	roots: ["<rootDir>/tests"],
 	coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/src/providers"],
+	modulePathIgnorePatterns: [
+		"<rootDir>/tests/integration test/tmpl.test",
+		"<rootDir>/tests/unit test/tmpl.test"
+	],
 	preset: "ts-jest",
 	moduleNameMapper: require("alias-hq").get("jest")
 }
