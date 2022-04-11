48baad58f0ab020b41a046a09395e9e11054e10e
Diego <diegoulloao@icloud.com>
2022-04-11 01:52:42 -0400

    fix(scripts): simpler scripts commands -> ts-paths/register moved inside

--- src/providers/core/jobs/route-list.ts
+++ src/providers/core/jobs/route-list.ts
@@ -7,14 +7,20 @@
  *  @author diegoulloao
  * 
  */
+
+import "tsconfig-paths/register"
+
 import routerDex from "router-dex/inspector"
+
+/*
+ *	Note: Require prevents missing module error by LSP
+ */
 
 const { express } = require("@services")
 const packageJson = require("package.json")
 
 // Types
 import Express from "express"
-
 
 // Express server instance
 const server: Express.Application = express.getApplication()
